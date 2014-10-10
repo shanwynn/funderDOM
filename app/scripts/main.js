27e221980aeee6785c11189f323bf1349be476a8
Justin Herrick <justin@justinherrick.com>
2014-10-10 16:16:22 -0400

    Update main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,11 +25,22 @@ var destination=document.querySelector('#colorTarget');
 destination.innerHTML=colorTemplate.join('');*/
 
 $('.title_1').on('mouseover', function (event) {
-$('.text_1').show('active');
+    $('.text_1').show('active'); // <-- Good
 });
+//It makes the file much more readable (and is idiomatic) to indent the lines of anything nested inside a function.
 $('.title_2').on('mouseout', function (event) {
-$('.text_2').hide('active');
+$('.text_2').hide('active');  // <--- Not as good
 });
+
+/*
+  The .show() and .hide() methods do not take a class argument. http://api.jquery.com/show/
+  So this will never do what you want it to do for you.
+  
+  It looks like you are trying to add and remove a class. You will do that with .addClass() and .removeClass()
+  
+  I'd also be careful when using mouseover/mouseout they tend to behave in ways you didn't expect.
+  
+*/
 
 
 $('.title_2').on('mouseover', function (event) {
